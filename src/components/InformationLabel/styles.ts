717b809ecc13a2ebbeac70ef78668521fbcb1795
Andy <anderson54644@gmail.com>
2023-11-23 19:46:30 -0300

    add borda, tamanho da fonte e peso do componente InformationLabel

--- src/components/InformationLabel/styles.ts
+++ src/components/InformationLabel/styles.ts
@@ -2,30 +2,32 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 interface IContentProps {
-  type: "COUNT" | "VALUE"
+  type: "COUNT" | "VALUE";
 }
-
 
 export const Container = styled.View``;
 
 export const Content = styled.View<IContentProps>`
   background-color: #f7cb46;
-  width: ${({type}) => type === "COUNT" ? RFValue(120) : RFValue(200)}px;
+  width: ${({ type }) => (type === "COUNT" ? RFValue(120) : RFValue(200))}px;
   height: ${RFValue(40)}px;
   z-index: 1;
   justify-content: center;
   align-items: center;
   border-radius: 20px;
+  border: 2px solid #000;
 `;
 
 export const Shadow = styled.View<IContentProps>`
   margin-left: 5px;
   margin-top: -33px;
-  width: ${({type}) => type === "COUNT" ? RFValue(120) : RFValue(200)}px;
+  width: ${({ type }) => (type === "COUNT" ? RFValue(120) : RFValue(200))}px;
   background-color: #000;
   height: ${RFValue(40)}px;
   border-radius: 20px;
 `;
 
 export const InfoText = styled.Text`
+  font-size: ${RFValue(16)}px;
+  font-weight: 500;
 `;
