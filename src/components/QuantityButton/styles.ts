9632bd60f035e0e0b3def3f2f45e171229a3b1b5
Andy <anderson54644@gmail.com>
2023-12-04 22:28:13 -0300

    alterando altura e largura do componente QuantityButton

--- src/components/QuantityButton/styles.ts
+++ src/components/QuantityButton/styles.ts
@@ -10,8 +10,8 @@ interface IContainerProps {
 }
 export const Container = styled.View<IContainerProps>`
 
-  width: ${RFValue(95, Dimensions.get('window').height)}px;
-  height: ${RFValue(55)}px;
+  width: ${RFValue(80, Dimensions.get('window').height)}px;
+  height: ${RFValue(30)}px;
   background-color: ${({ type }) => type === 'add' ? '#0CCA4A' : '#E24330'};
   border-radius: 50px;
   z-index: 1;
@@ -21,25 +21,25 @@ export const Container = styled.View<IContainerProps>`
 `;
 
 export const Content = styled.TouchableOpacity`
-  width: ${RFValue(95, Dimensions.get('window').height)}px;
+  width: ${RFValue(80, Dimensions.get('window').height)}px;
   
-  height: ${RFValue(55)}px;
+  height: ${RFValue(30)}px;
   justify-content: center;
 `;
 
 export const Shadow = styled.View`
-  width: ${RFValue(95, Dimensions.get('window').height)}px;
+  width: ${RFValue(80, Dimensions.get('window').height)}px;
   
-  height: ${RFValue(55)}px;
+  height: ${RFValue(30)}px;
   background-color: #000;
   border-radius: 50px;
-  margin-top: ${RFValue(-50)}px;
+  margin-top: ${RFValue(-25)}px;
   margin-left: ${RFValue(5)}px;
 `;
 
 export const Icon = styled(Feather)`
   text-align: center;
-  font-size: ${RFValue(25)}px;
+  font-size: ${RFValue(18)}px;
   color: #fff;
 `;
 
