9cbc5e0990d41ae590267b70b25d1f3c9217f038
Andy <anderson54644@gmail.com>
2023-12-11 10:34:37 -0300

    refatorando estilo do componente InformationLabel

--- src/components/InformationLabel/styles.ts
+++ src/components/InformationLabel/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.View``;
 
 export const Content = styled.View<IContentProps>`
   background-color: #f7cb46;
-  width: ${({ type }) => (type === "COUNT" ? RFValue(120) : RFValue(200))}px;
+  width: ${({ type }) => (type === "COUNT" ? RFValue(120) : RFValue(150))}px;
   height: ${RFValue(40)}px;
   z-index: 1;
   justify-content: center;
@@ -21,7 +21,7 @@ export const Content = styled.View<IContentProps>`
 export const Shadow = styled.View<IContentProps>`
   margin-left: 5px;
   margin-top: ${RFValue(-33)}px;
-  width: ${({ type }) => (type === "COUNT" ? RFValue(120) : RFValue(200))}px;
+  width: ${({ type }) => (type === "COUNT" ? RFValue(120) : RFValue(150))}px;
   background-color: #000;
   height: ${RFValue(40)}px;
   border-radius: 20px;
