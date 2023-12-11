709e9afe3c6e6bb256fc1ed77009a86a9e8d186d
Andy <anderson54644@gmail.com>
2023-12-11 10:37:36 -0300

    refatorando componente QuantityButton adicionando evento onPress

--- src/components/QuantityButton/index.tsx
+++ src/components/QuantityButton/index.tsx
@@ -4,13 +4,14 @@ import { Container, Content, Icon, Shadow, Wrapper } from "./styles";
 
 interface IProps {
   type: 'add' | 'remove'
+  action: (type: 'add'| 'remove') => void;
 }
 
-export const QuantityButton = ({ type } : IProps) => {
+export const QuantityButton = ({ type, action } : IProps) => {
   return (
     <Wrapper>
       <Container type={type}>
-        <Content>
+        <Content onPress={() => action(type)}>
           <Icon name={type === 'add' ? 'plus' : 'minus'} />
         </Content>
       </Container>
